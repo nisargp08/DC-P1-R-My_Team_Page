aed2eea2144f8505c398ad5887aa097a19a92cad
Nisarg Patel <nisargp42@gmail.com>
2020-09-10 20:29:37 -0400

    User story :: Done

--- src/App.vue
+++ src/App.vue
@@ -3,61 +3,61 @@
     <!-- Wrapper -->
     <div class="flex flex-col min-h-screen h-full lg:max-w-screen-lg mx-auto">
         <!-- Header -->
-        <header class="lg:flex">
-            <div class="lg:w-1/2">
-                <h1 class="font-ptserif font-bold text-4xl leading-2x-loose text-left align-top tracking-tighter py-4 px-3 text-black">The creative crew</h1>
+        <header class="lg:flex lg:pt-24">
+            <div class="lg:flex lg:justify-center lg:w-1/2 xl:justify-start">
+                <h1 class="font-ptserif font-bold text-4xl leading-2x-loose text-left align-top tracking-tighter py-4 px-3 text-black xl:flex-1">The creative crew</h1>
             </div>
             <div class="mt-4 px-3 lg:w-1/2">
-                <h2 class="uppercase font-poppins font-semibold text-lg text-left align-top leading-5 text-black">Who we are</h2>
-                <p class="font-poppins font-normal text-sm text-left align-top leading-5 mt-3 mr-8 text-black">We are team of creatively diverse. driven. innovative individuals working in various locations from the world.</p>
+                <h2 class="uppercase font-poppins font-semibold text-lg text-left align-top leading-7 text-black">Who we are</h2>
+                <p class="font-poppins font-normal text-sm text-left align-top leading-5 mt-3 mr-8 text-black lg:w-76 lg:text-lg lg:leading-7">We are team of creatively diverse. driven. innovative individuals working in various locations from the world.</p>
             </div>
         </header>
 
         <!-- Main -->
-        <main class="flex-1 my-16">
-            <div class="max-w-lg sm:max-w-lg sm:mx-auto lg:mx-0">
+        <main class="flex-1 my-16 lg:my-32 lg:flex">
+            <div class="max-w-lg sm:max-w-lg sm:mx-auto lg:mx-0 lg:max-w-full">
                 <div class="flex flex-wrap justify-around">
-                    <div class="w-2/5">
+                    <div class="w-2/5 lg:w-26">
                         <div class="flex">
                             <img src="@/assets/images/photo1.png" alt="Bill Mahoney">
-                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black">Product Owner</div>
+                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black lg:text-xs lg:leading-4">Product Owner</div>
                         </div>
-                        <span class="inline-block font-poppins pt-3 font-semibold font-sm text-left align-top leading-5 text-black">Bill Mahoney</span>
+                        <span class="inline-block font-poppins text-sm pt-3 font-semibold font-sm text-left align-top leading-5 lg:text-lg lg:leading-7 text-black">Bill Mahoney</span>
                     </div>
-                    <div class="w-2/5 mt-12">
+                    <div class="w-2/5 lg:w-26 mt-12 lg:mt-32">
                         <div class="flex">
                             <img src="@/assets/images/photo2.png" alt="Saba Cabrera">
-                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black">Art Director</div>
+                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black lg:text-xs lg:leading-4">Art Director</div>
                         </div>
-                        <span class="inline-block font-poppins pt-3 font-semibold font-sm text-left align-top leading-5 text-black">Saba Cabrera</span>
+                        <span class="inline-block font-poppins text-sm pt-3 font-semibold font-sm text-left align-top leading-5 lg:text-lg lg:leading-7 text-black">Saba Cabrera</span>
                     </div>
-                    <div class="w-2/5">
+                    <div class="w-2/5 lg:w-26">
                         <div class="flex">
                             <img src="@/assets/images/photo3.png" alt="Shae Le">
-                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black">Tech Lead</div>
+                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black lg:text-xs lg:leading-4">Tech Lead</div>
                         </div>
-                        <span class="inline-block font-poppins pt-3 font-semibold font-sm text-left align-top leading-5 text-black">Shae Le</span>
+                        <span class="inline-block font-poppins text-sm pt-3 font-semibold font-sm text-left align-top leading-5 lg:text-lg lg:leading-7 text-black">Shae Le</span>
                     </div>
-                    <div class="w-2/5 mt-12">
+                    <div class="w-2/5 lg:w-26 mt-12">
                         <div class="flex">
                             <img src="@/assets/images/photo4.png" alt="Skylah Lu">
-                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black">UX Designer</div>
+                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black lg:text-xs lg:leading-4">UX Designer</div>
                         </div>
-                        <span class="inline-block font-poppins pt-3 font-semibold font-sm text-left align-top leading-5 text-black">Skylah Lu</span>
+                        <span class="inline-block font-poppins text-sm pt-3 font-semibold font-sm text-left align-top leading-5 lg:text-lg lg:leading-7 text-black">Skylah Lu</span>
                     </div>
-                    <div class="w-2/5">
+                    <div class="w-2/5 lg:w-26 lg:mt-32">
                         <div class="flex">
                             <img src="@/assets/images/photo5.png" alt="Griff Richards">
-                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black">Developer</div>
+                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black lg:text-xs lg:leading-4">Developer</div>
                         </div>
-                        <span class="inline-block font-poppins pt-3 font-semibold font-sm text-left align-top leading-5 text-black">Griff Richards</span>
+                        <span class="inline-block font-poppins text-sm pt-3 font-semibold font-sm text-left align-top leading-5 lg:text-lg lg:leading-7 text-black">Griff Richards</span>
                     </div>
-                    <div class="w-2/5 mt-12">
+                    <div class="w-2/5 lg:w-26 mt-12">
                         <div class="flex">
                             <img src="@/assets/images/photo6.png" alt="Stan John">
-                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black">Developer</div>
+                            <div class="vertical-text font-ptserif text-xxs uppercase align-top leading-3 text-left font-normal text-black lg:text-xs lg:leading-4">Developer</div>
                         </div>
-                        <span class="inline-block font-poppins pt-3 font-semibold font-sm text-left align-top leading-5 text-black">Stan John</span>
+                        <span class="inline-block font-poppins text-sm pt-3 font-semibold font-sm text-left align-top leading-5 lg:text-lg lg:leading-7 text-black">Stan John</span>
                     </div>
                 </div>
             </div>
@@ -65,7 +65,7 @@
         <!-- Footer -->
         <footer>
             <div class="flex justify-center items-center">
-                <div class="author font-montserrat font-semibold pb-6">Nisarg Patel @ DevChallenges.io</div>
+                <div class="author font-montserrat font-semibold pb-6 text-sm leading-4">Nisarg Patel @ DevChallenges.io</div>
             </div>
         </footer>
     </div>
--- tailwind.js
+++ tailwind.js
@@ -2,19 +2,23 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
-      fontFamily : {
-        montserrat : ['Montserrat','sans-serif'],
-        poppins : ['Poppins','sans-serif'],
-        ptserif : ['PT Serif','serif'],
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        ptserif: ["PT Serif", "serif"],
       },
-      lineHeight : {
-        '2x-loose' : '3rem'
+      lineHeight: {
+        "2x-loose": "3rem",
       },
-      fontSize : {
-        'xxs' : '10px'
-      }
+      fontSize: {
+        xxs: "10px",
+      },
+      width: {
+        "26": "26%",
+        "76" : "26rem",
+      },
     },
   },
   variants: {},
   plugins: [],
-}
+};
